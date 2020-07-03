c4736d0672aea456b358581ea867d1e0037ec3ff
Antoine Chemin <antoine.chemin.pro@outlook.fr>
2020-07-03 10:54:38 +0200

    refactoring of first issue, simple mod

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-// alert("bonjour et bienvenue");
-// var userChoice = prompt("eau ou coca"); 
-// console.log(userChoice);
-// alert("Vous avez choisi " + userChoice);
-
 alert("bonjour et bienvenue ! penses tu avoir le talent de me battre?");
 var userName = prompt ("Quel est ton nom?");
-console.log(userName);
 // // ask user name and stock it
 
 function goodLuck ()
@@ -14,46 +8,47 @@ function goodLuck ()
 };
 userChoice = goodLuck(userName);
 userChoice = userChoice.toLowerCase();
-console.log ("choix du joueur : " + userChoice);
 // // ask user choice and stock it caseFriendly
 
 var possible = ["pierre", "feuille", "ciseau"];
-var test = Math.floor(Math.random()*3);
-console.log(test);
 // list of the different choice, user or computer
 
 var computerChoice = possible[Math.floor(Math.random()*3)];
 console.log("choix de l'ordinateur : " + computerChoice);
 // choose in array by index randomly
 
-if (computerChoice === userChoice)
-{
-    alert ("Egalité !");
-}
-else if (computerChoice === "pierre" && userChoice === "feuille")
-{
-    
-    alert("gagné, j'ai choisi : " + computerChoice);
-}
-else if (computerChoice === "pierre" && userChoice === "ciseau")
-{
-    alert("perdu, j'ai choisi : " + computerChoice);
-}
-else if (computerChoice === "ciseau" && userChoice === "feuille")
-{
-    alert("perdu, j'ai choisi : " + computerChoice);
-}
-else if (computerChoice === "ciseau" && userChoice === "pierre")
-{
-    alert("gagné, j'ai choisi : " + computerChoice);
-}
-else if (computerChoice === "feuille" && userChoice === "pierre")
-{
-    alert("perdu, j'ai choisi : " + computerChoice);
-}
-else if (computerChoice === "feuille" && userChoice === "ciseau")
-{
-    alert("gagné, j'ai choisi : " + computerChoice);
-};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var conditionDeVictoire =
+[
+    {homme : "pierre", machine : "ciseau"},
+    {homme : "feuille", machine : "pierre"},
+    {homme : "ciseau", machine : "feuille"}
+];
+var victoire = victory (conditionDeVictoire);
 
-
+function victory (conditionDeVictoire)
+{
+    for (var key in conditionDeVictoire)
+    {
+        if (conditionDeVictoire[key]["homme"] === userChoice  
+            && conditionDeVictoire[key]["machine"] === computerChoice)
+            {
+        return true;
+            }
+    }
+        return false;
+}
+// do the set between computer and user choices
+if (userChoice === computerChoice)
+{
+    alert("égalité, tu as eu de la chance, mais je t'aurai !");
+}
+else if (victoire === true)
+{
+    alert("Tu as gagné ce point, tricherais-tu?");
+}
+else
+{
+    alert("tu as perdu, abandonne avant qu'il ne soit trop tard !");
+}
+// taunt user about the victory
