8779629cf1759142758510ffb04c641e80b31fa8
AntoineCheminPro <antoine.chemin.pro@outlook.fr>
2020-07-03 19:14:26 +0200

    game is complete, but need some refactoring to be cleanest

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert("bonjour et bienvenue ! penses tu avoir le talent de me battre?");
+alert("bonjour et bienvenue ! penses tu avoir la chance de me battre?");
 // welcome to user
 
 var userName = prompt ("Quel est ton nom?");
@@ -14,75 +14,84 @@ while (userName.length > 20 || userName.length <=1)
     }
 };
 // ask user name and stock it if length between 2 and 20 else ask again
-var tryAgain = true;
 var scoreHumain = 0;
 var scoreMachine = 0;
-
+var userChoice = 0;
 do
 {
+    scoreHumain = 0;
+    scoreMachine = 0;
+    userChoice = 0;
     do
-    {    
-   var userChoice = prompt (userName + " ,que choisis-tu? pierre feuille ou ciseau?");
-   userChoice = userChoice.toLowerCase();
-    }
-    while (userChoice !== "pierre" && userChoice !== "feuille" && userChoice !== "ciseau");
-    // ask user choice and stock it caseFriendly as it's one of the three good words
-
-    var possible = ["pierre", "feuille", "ciseau"];
-    // list of the differents choices, user or computer
-
-    var computerChoice = possible[Math.floor(Math.random()*3)];
-    console.log("choix de l'ordinateur : " + computerChoice);
-    // choose in array by index randomly
-
-    var conditionDeVictoire =
-    [
-        {homme : "pierre", machine : "ciseau"},
-        {homme : "feuille", machine : "pierre"},
-        {homme : "ciseau", machine : "feuille"}
-    ];
-    var victoire = victory (conditionDeVictoire);
-
-    function victory (conditionDeVictoire)
     {
-        for (var key in conditionDeVictoire)
+        do
+        {    
+            userChoice = prompt (userName + " ,que choisis-tu? pierre feuille ou ciseau?");
+            userChoice = userChoice.toLowerCase();
+    }
+        while (userChoice !== "pierre" && userChoice !== "feuille" && userChoice !== "ciseau");
+        // ask user choice and stock it caseFriendly as it's one of the three good words
+
+        var possible = ["pierre", "feuille", "ciseau"];
+        // list of the differents choices, user or computer
+
+        var computerChoice = possible[Math.floor(Math.random()*3)];
+        console.log("choix de l'ordinateur : " + computerChoice);
+        // choose in array by index randomly
+
+        var conditionDeVictoire =
+        [
+            {homme : "pierre", machine : "ciseau"},
+            {homme : "feuille", machine : "pierre"},
+            {homme : "ciseau", machine : "feuille"}
+        ];
+        var victoire = victory (conditionDeVictoire);
+        // show the 3 way to earn a point for user
+
+        function victory (conditionDeVictoire)
         {
-            if (conditionDeVictoire[key]["homme"] === userChoice  
-                && conditionDeVictoire[key]["machine"] === computerChoice)
+            for (var key in conditionDeVictoire)
             {
+                if (conditionDeVictoire[key]["homme"] === userChoice && conditionDeVictoire[key]["machine"] === computerChoice)
+                {
             return true;
             }
         }
-        return false;
+            return false;
     };
-    var afficheScore = 0;
-    // do the set between computer and user choices return true if user win else false
-    if (userChoice === computerChoice)
-    {
-        afficheScore = ("score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
-        alert("j'ai choisi : "+ computerChoice+ " égalité, tu as eu de la chance, mais je t'aurai !");
-        alert(afficheScore);
-    }
-    else if (victoire === true)
-    {
-        scoreHumain ++; 
-        afficheScore = ("score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
-        alert("j'ai choisi : "+ computerChoice+ " Tu as gagné ce point, tricherais-tu?");
-        alert(afficheScore);
-    }
-    else
-    {
-        scoreMachine ++;
-        afficheScore = ("score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
-        alert("j'ai choisi : "+ computerChoice+ " tu as perdu, abandonne avant qu'il ne soit trop tard !");
-        alert(afficheScore);
-    }
-    // if (scoreHumain == 3 || scoreMachine == 3)
-    // {
-    //     alert ("partie finie");
-    // };
-}while (scoreHumain < 3 && scoreMachine < 3);
-console.log( "h" + scoreHumain);
-console.log( "m" + scoreMachine);
+        var afficheScore = 0;
+        var playAgain = 0;
+        // do the set between computer and user choices return true if user win else false
+        if (userChoice === computerChoice)
+        {
+            afficheScore = ("\n score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
+            alert("j'ai choisi : "+ computerChoice + " égalité, tu as eu de la chance, mais je t'aurai !" + afficheScore);
+        }
+        else if (victoire === true)
+        {
+            scoreHumain ++;
+            afficheScore = ("\n score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
+            alert("j'ai choisi : "+ computerChoice+ " Tu as gagné ce point, tricherais-tu? \n" + afficheScore);
+        }
+        else
+        {
+            scoreMachine ++;
+            afficheScore = (" \n score : " + userName + " " + scoreHumain + "     score machine : " + scoreMachine); 
+            alert("j'ai choisi : " + computerChoice + " tu as perdu, abandonne avant qu'il ne soit trop tard ! " + afficheScore);
+        }
+        if (scoreHumain == 3)
+        {
+            alert ("partie finie, je m'incline, tu as gagné ! BRAVO " + userName);
+        }
+        else if (scoreMachine == 3)
+        {
+            alert ("HAHAHA ! je te l'avais dit que j'allais gagner, je dois reconnaître que tu t'es bien défendu " + userName)
+        };
+    } while (scoreHumain < 3 && scoreMachine < 3);
+    // end of the match as a competitor has 3 points
 
-// while (tryAgain = false);
+    var playAgain = window.confirm(" On rejoue ça en 3 manches gagnantes?");
+} while (playAgain == true);
+// let choice to play again
+
+
